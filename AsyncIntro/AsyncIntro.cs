90905483a892697675e7696a1c931f67db1c47a1
epikkoder <ronaldsahagun87@gmail.com>
2021-10-09 13:04:03 -0700

    Initial commit

---
+++ AsyncIntro/AsyncIntro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace AsyncIntro
+{
+    public partial class AsyncIntro : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private readonly Label label;
+        private readonly Button button;
+
+        public AsyncIntro()
+        {
+            label = new Label
+            {
+                Location = new Point(10, 20),
+                Text = "Length"
+            };
+
+            button = new Button
+            {
+                Location = new Point(10, 50),
+                Text = "Click"
+            };
+
+            button.Click += DisplayWebSiteLength;
+            AutoSize = true;
+            Controls.Add(label);
+            Controls.Add(button);
+        }
+
+        private async void DisplayWebSiteLength(object sender, EventArgs e)
+        {
+            label.Text = "Fetching...";
+            string text = await client.GetStringAsync("http://csharpindepth.com");
+            label.Text = text.Length.ToString();
+        }
+    }
+}
